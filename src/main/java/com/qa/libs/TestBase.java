4e1a646ee9f6f04087639abebd37208752903bbc
Bivash <bivash.cts@gmail.com>
2021-04-23 09:59:06 +0530

    system updated with System in local

--- src/main/java/com/qa/libs/TestBase.java
+++ src/main/java/com/qa/libs/TestBase.java
@@ -4,7 +4,7 @@ public class TestBase {
 	
 	public void testbase() {
 		System.out.println("test base class");
-		system.out.println("Another note added from github itself");
+		System.out.println("Another note added from github itself");
 		
 	}
 
